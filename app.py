4e76fa46805c4a8102f0bf8d66c0d721172b8214
Katia Bellido <katiabellido@MacBook-Pro-de-Katia.local>
2021-11-21 20:21:47 -0600

    login page

--- app.py
+++ app.py
@@ -10,3 +10,7 @@ app.secret_key = os.environ.get('secret_key')
 @app.route('/')
 def index():
     return render_template('/index.html')
+
+@app.route('/login')
+def login():
+    return render_template('/login.html')

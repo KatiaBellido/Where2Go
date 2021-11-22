5a739e090e0c2ca528e4d6bb2edfe62cad1c53fb
Katia Bellido <katiabellido@MacBook-Pro-de-Katia.local>
2021-11-21 20:10:33 -0600

    Updates and cleans

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask,render_template, request, session, url_for
+
+import os
+import datetime
+
+app = Flask(__name__)
+app.permanent_session_lifetime = datetime.timedelta(days=360)
+app.secret_key = os.environ.get('secret_key')
+
+@app.route('/')
+def index():
+    return render_template('/index.html')
